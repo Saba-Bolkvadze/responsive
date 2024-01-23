a864bd1ad3bee33852ee116b68b3e8d4dda94b6a
Tornike Romanadze <tokaromanadze1997@gmail.com>
2024-01-23 19:42:29 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// function test() {
+//     console.log("1");
+//     console.log("2");
+//     console.log("3");
+
+    
+// }
+
+// test();
+
+
+// for (i = 0; i <= 5; i++   ) {
+//     test()
+// }
+
+
+// function misalmeba(saxeli,asaki) {
+//     alert(`გამარჯობა ${saxeli} შენ ხარ ${asaki} წლის`)
+// }
+
+
+// let saxeli = prompt("შეიყვანე სახელი")
+// let asaki = prompt("შეიყვანე ასაკი")
+
+// misalmeba(saxeli, asaki);
+
+// function mimateba(a, b) {
+//   let plus = a + b;
+//   return plus;
+
+  
+// }
+
+// console.log(mimateba(2, 5));
+
+// let globaluri = "მე ვარ გლობალური ცვლადი"
+                                                                                                
+// function testFunction() {
+//     console.log(globaluri)
+// }
+// testFunction()
+// console.log(globaluri)
+
+// function gamravleba(a, b, c) {
+//     return a * b * c
+// }
+
+// let pasuxi = gamravleba(4, 1, 2)
+// console.log(pasuxi)
+
+
+// ARROW FUNCTIONS
+
+// let gamravleba = (x,y) => x * y;
+
+// let pasuxi = gamravleba(3,4)
+// console.log(pasuxi)  
